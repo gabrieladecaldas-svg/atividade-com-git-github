c2307b9ddf6730f2ef1b5718127ff220285c129a
gabriella-de-caldas <gabrieladecaldas@gmail.com>
2026-03-18 13:19:59 -0300

    Meu primeiro commit

---
+++ condicao-simples/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("digite  sua idade:"));
+let temCNH = prompt("possui CNH?(sim ou nao")
+if(idade >= 18 && tem CNH ==="sim") {  
+      alert("Pode dirigir");
+} else  {
+       alert("não pode dirigir"); 
+    }
+    
